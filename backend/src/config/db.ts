cc9c46859814f98f7c69c79bd83dd4d6b56fbfae
Amar Deep <amard0819@gmail.com>
2026-06-03 21:20:50 +0530

    Deploy to render back

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,17 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const isProduction =
-  process.env.NODE_ENV === "production";
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-
-  ssl: isProduction
-    ? {
-        rejectUnauthorized: false,
-      }
-    : false,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export default pool;
